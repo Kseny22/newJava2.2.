703164c333003e2f86f07841ae2f5f30a8e4c10c
Emma Paris <misyura.c@yandex.ru>
2024-07-09 23:40:10 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,32 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.print("На счету у клиента: 300 рублей." );
-        System.out.print("Сумма пополнения счета: 1700 рублей." );
+        System.out.print("На счету у клиента: 300 рублей.");
+        System.out.print("Сумма пополнения счета: 1700 рублей.");
 
 
         double amount = 1700;
         double initialAccount = 300;
-
-
         if (amount > 1000) {
 
-        int fullHundreds = (int) Math.floor(amount / 100);
-        double bonus = fullHundreds * 1;
+            int fullHundreds = (int) Math.floor(amount / 100);
+            double bonus = fullHundreds * 1;
 
 
-        double totalAmount = amount + bonus + initialAccount;
+            double totalAmount = amount + bonus + initialAccount;
 
 
-        System.out.println("Итоговая сумма на счету клиента: " + totalAmount + "рублей");
-        System.out.println("Количество бонусных рублей: " + bonus + " рублей");
+            System.out.println("Итоговая сумма на счету клиента: " + totalAmount + "рублей");
+            System.out.println("Количество бонусных рублей: " + bonus + " рублей");
 
 
         } else {
 
 
-        System.out.println("Бонусов нет");
+            System.out.println("Бонусов нет");
 
 
-        System.out.println("Итоговая сумма на счету клиента; " + amount + " рублей");
-
+            System.out.println("Итоговая сумма на счету клиента; " + amount + " рублей");
 
 
         }
