01d309d3f7fda45cb62b4f63449101d9a06b4453
Emma Paris <misyura.c@yandex.ru>
2024-07-11 23:47:01 +0300

    README.md

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-
         System.out.print("На счету у клиента: 100 рублей.");
-        System.out.print("Сумма пополнения счета:1100 рублей.");
+        System.out.print("Сумма пополнения счета: 300 рублей.");
 
-
-        int addend = 1100;
         int initialBalance = 100;
+        int addend = 300;
 
         int bonus = 0;
         if (addend > 1000) {
             bonus = addend / 100;
-
-
-            int finalBalance = addend + bonus + initialBalance;
-
-
-            System.out.println("Итоговая сумма на счету клиента: " + finalBalance + "рублей");
-            System.out.println("Количество бонусных рублей: " + bonus + " рублей");
-
-
-        } else {
-
-
-            System.out.println("Бонусов нет");
-
-
-            System.out.println("Итоговая сумма на счету клиента; " + addend + " рублей");
-
-
         }
+
+        int finalBalance = initialBalance + addend + bonus;
+        System.out.println("Итоговый баланс" + finalBalance);
+        System.out.println("Бонус" + bonus);
     }
 }
